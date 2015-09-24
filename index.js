44290cc3eb1d3b5245b9291607a5da33952a9ed5
Mike James <m.l.james@me.com>
2015-09-24 15:47:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
 
 var mapObj = require('map-obj');
 
-var internals = {};
+function isObject (v){
+	return typeof(v) === 'object';
+}
+
+function camelCase(str) {
+    return str.replace(/[_.-](\w|$)/g, function (_,x) {
+        return x.toUpperCase();
+    });
+}
+
 
 function camelCaseRecursive(obj) {
 	return mapObj(obj, function(key, val) {
@@ -11,38 +20,29 @@ function camelCaseRecursive(obj) {
 		if (Array.isArray(val)) {
 
 			val.forEach(function(value) {
-				if(internals.isObject(value) && !Array.isArray(value)){
+				if(isObject(value) && !Array.isArray(value)){
 					newArray.push(camelCaseRecursive(value));					
 				}else{
 					newArray.push(value);
 				}
 			});
 
-			return [internals.camelCase(key), newArray];
+			return [camelCase(key), newArray];
 
 		}else if(!val){
-			return [internals.camelCase(key), val];
+			return [camelCase(key), val];
 		} 
-		else if (internals.isObject(val)) {
+		else if (isObject(val)) {
 
-			return [internals.camelCase(key), camelCaseRecursive(val)];
+			return [camelCase(key), camelCaseRecursive(val)];
 
 		} else {
 
-			return [internals.camelCase(key), val];
+			return [camelCase(key), val];
 
 		}
 	});
 }
 
-internals.isObject = function (v){
-	return typeof(v) === 'object';
-}
-
-internals.camelCase = function(str) {
-    return str.replace(/[_.-](\w|$)/g, function (_,x) {
-        return x.toUpperCase();
-    });
-}
 
 module.exports = camelCaseRecursive;
