b1c249b36d416e51a4cdb7d237083024ca9a8df6
Mike James <m.l.james@me.com>
2015-11-10 16:30:55 +0000

    Removed unecessary code

--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ const isObject = (v) => typeof(v) === 'object';
 
 const camelCase = (str) => str.replace(/[_.-](\w|$)/g, (_, x) => x.toUpperCase());
 
-const bob = {
-  name: 'bob'
-};
-
-bob.name = 'tim';
-
 function camelCaseRecursive(obj) {
+
   return mapObj(obj, (key, val) => {
     const newArray = [];
 
