d21de21c5d6b5d2cc5f6961bf03b9d0cf121c93a
Mike James <m.l.james@me.com>
2015-11-10 12:30:08 +0000

    moved to es15 added build step for babel compile

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import mapObj from 'map-obj';
+
+const isObject = (v) => typeof(v) === 'object';
+
+const camelCase = (str) => str.replace(/[_.-](\w|$)/g, (_, x) => x.toUpperCase());
+
+
+function camelCaseRecursive(obj) {
+  return mapObj(obj, (key, val) => {
+    const newArray = [];
+
+    if (Array.isArray(val)) {
+      val.forEach((value) => {
+        if (isObject(value) && !Array.isArray(value)) {
+          newArray.push(camelCaseRecursive(value));
+        } else {
+          newArray.push(value);
+        }
+      });
+
+      return [camelCase(key), newArray];
+    } else if (!val) {
+      return [camelCase(key), val];
+    } else if (val instanceof Date) {
+      return [camelCase(key), val];
+    } else if (isObject(val)) {
+      return [camelCase(key), camelCaseRecursive(val)];
+    }
+
+    return [camelCase(key), val];
+  });
+}
+
+export default camelCaseRecursive;
